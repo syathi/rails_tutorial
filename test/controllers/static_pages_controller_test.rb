20e5c37dee690297f7431dc4a4190c4e076e955e
syathi <femto5401@gmail.com>
2018-04-13 00:05:17 +0900

    add sass gem

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | RailsTutorial"
   end
 
   test "should get help" do
