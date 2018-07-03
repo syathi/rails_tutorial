ae1c85995b8bd0931f220db062c275c1d5b01cf2
syathi <femto5401@gmail.com>
2018-07-03 23:29:04 +0900

    done 13.3.2

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,19 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy]
 
-    def create
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
     end
+  end
 
-    def destroy
+  def destroy
+  end
+
+  private 
+    def micropost_params
+      params.require(:micropost).permit(:content)
     end
 end
