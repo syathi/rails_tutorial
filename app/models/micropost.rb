393ec403dc18ae01f3bb6e1ad3ea1fcb0e4d6601
syathi <femto5401@gmail.com>
2018-07-14 01:11:12 +0900

    done 14.1.3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,10 +1,10 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc)}
-  mount_uploader :picture, PictureUploader
+  mount_uploader(:picture, PictureUploader)
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
-  validates :picture_size
+  validate :picture_size
 
   private
     #画像サイズのバリデーション
